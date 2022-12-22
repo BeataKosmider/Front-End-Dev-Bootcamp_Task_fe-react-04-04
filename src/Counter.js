192d0ecc8a20953d7f3dc90993fae877c4d690c6
Krzysztof Mendrek <kmendrek94@gmail.com>
2022-12-22 12:27:50 +0100

    Task update

--- src/Counter.js
+++ src/Counter.js
@@ -1,28 +1,5 @@
 import React from 'react';
 
-class Counter extends React.Component {
-  constructor(props) {
-    super();
-
-    this.state = {
-      ticks: 0,
-      isBtnDisabeld: false
-    }
-  }
-
-  incrementTicks = () => {
-    let { ticks } = this.state;
-    ticks++;
-    this.setState({ ticks }, () => {
-      if(this.state.ticks === 10) {
-        this.setState({isBtnDisabeld: true});
-      }
-    })
-  }
-
-  render() {
-    return <button disabled={this.state.isBtnDisabeld} onClick={this.incrementTicks}>{this.state.ticks}</button>;
-  }
-}
+class Counter extends React.Component {}
 
 export default Counter;
